3a9d10d97d673931f26a5be8e5be9eed1bbeefa3
lisz1012 <lisz1012@163.com>
2021-01-15 00:53:48 -0800

    配置Druid的监控界面：1.加log4j 2.yml中加配置 3.Configuration类里面加一个Servlet的Bean

--- src/main/java/com/lisz/config/DruidConfig.java
+++ src/main/java/com/lisz/config/DruidConfig.java
@@ -1,11 +1,19 @@
 package com.lisz.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.Filter;
+import javax.servlet.Servlet;
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class DruidConfig {
@@ -13,5 +21,27 @@ public class DruidConfig {
 	@Bean
 	public DataSource druidDataSource(){
 		return new DruidDataSource();
+	}
+
+	@Bean
+	public ServletRegistrationBean<Servlet> druidServletRegistrationBean(){
+		ServletRegistrationBean<Servlet> servletRegistrationBean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
+		Map<String, String> initParams = new HashMap<>();
+		initParams.put("loginUsername", "admin");
+		initParams.put("loginPassword", "123456");
+		initParams.put("allow", "");
+		servletRegistrationBean.setInitParameters(initParams);
+		return servletRegistrationBean;
+	}
+
+	// 过滤掉不必要的请求，不用这个过滤器也没关系
+	@Bean
+	public FilterRegistrationBean<Filter> webStatFilter(){
+		FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>();
+		bean.setFilter(new WebStatFilter());
+		Map<String, String> initParams = new HashMap<>();
+		initParams.put("exclusions", "*.js,*.css,/druid/*");
+		bean.setInitParameters(initParams);
+		return bean;
 	}
 }
