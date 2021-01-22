c40a094531411f69d970b05d4e9e8a5c6a3baf67
lisz1012 <lisz1012@163.com>
2021-01-22 02:29:34 -0800

    添加笔记

--- src/main/java/com/lisz/SpringbootDataApplication.java
+++ src/main/java/com/lisz/SpringbootDataApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class) //避免循环引用
 // 各种Component类，必须放在比这个main Class级别更低的包里，否则就要加@ComponentScan来指定
 // SpringBootApplication类的注解怎么最后把选择器拿出来，是要解决的问题
+// SpringApplication类中的prepareContext()和refreshContext()是重点
 public class SpringbootDataApplication {
 
 	public static void main(String[] args) {
