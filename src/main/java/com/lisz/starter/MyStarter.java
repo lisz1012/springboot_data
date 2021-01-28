3b87eb7a02a42b75c61c659e8131f5bbda23f21a
lisz1012 <lisz1012@163.com>
2021-01-28 01:22:38 -0800

    自动装配MyStarter类

--- src/main/java/com/lisz/starter/MyStarter.java
+++ src/main/java/com/lisz/starter/MyStarter.java
@@ -1,7 +1,6 @@
 package com.lisz.starter;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -12,7 +11,6 @@ public class MyStarter {
 		System.out.println("MyStarter inited ... ");
 	}
 
-	@Bean
 	public Person person(){
 		return new Person();
 	}
